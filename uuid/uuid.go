aac9a6d37e51326708e7d6ab9e0c578005e74dac
hzwy23 <hzwy23@163.com>
2017-11-08 15:47:17 +0800

    update uuid api

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -4,10 +4,12 @@ import (
 	"github.com/satori/go.uuid"
 )
 
+// 采用随机数的方式生成uuid
 func Random() string {
 	return uuid.NewV4().String()
 }
 
+// 采用随机数和sha1组合的方式生成uuid
 func UUID() string {
 	uid1 := uuid.NewV1().String()
 	uid2 := uuid.NewV4()
