986797825ea026e04c3695c32ef12f97b915c334
hzwy23 <hzwy23@163.com>
2017-11-08 15:46:05 +0800

    update uuid api

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -1,7 +1,15 @@
 package uuid
 
-import "github.com/satori/go.uuid"
+import (
+	"github.com/satori/go.uuid"
+)
 
-func GenUUID() string {
+func Random() string {
 	return uuid.NewV4().String()
+}
+
+func UUID() string {
+	uid1 := uuid.NewV1().String()
+	uid2 := uuid.NewV4()
+	return uuid.NewV5(uid2, uid1).String()
 }
---
+++ uuid/uuid_test.go
@@ -0,0 +1,19 @@
+package uuid_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hzwy23/utils/uuid"
+)
+
+func TestUuid(t *testing.T) {
+	fmt.Println(uuid.Random())
+	fmt.Println(uuid.Random())
+	fmt.Println(uuid.Random())
+	fmt.Println(uuid.Random())
+
+	fmt.Println(uuid.UUID())
+	fmt.Println(uuid.UUID())
+	fmt.Println(uuid.UUID())
+}
