21d78d75aecadeaab5620a93285501943d075908
hzwy23 <hzwy23@163.com>
2017-11-10 23:10:47 +0800

    add example code

---
+++ jwt/doc.go
@@ -0,0 +1,34 @@
+// json web token
+//
+// 示例代码：
+//	package jwt_test
+//
+//	import (
+//		"testing"
+//
+//		"fmt"
+//
+//		"github.com/hzwy23/panda/jwt"
+//	)
+//
+//	func TestToken(t *testing.T) {
+//		token, err := jwt.GenToken(jwt.NewUserdata().SetUserId("huang").SetOrgunitId("100"))
+//		fmt.Println(token, err)
+//		claims, err := jwt.ParseToken(token)
+//		fmt.Println(claims, err)
+//
+//		flag := jwt.ValidToken(token)
+//		fmt.Println("true or false:", flag)
+//
+//		handle := jwt.NewHandle(nil)
+//		flag = handle.ValidToken(token)
+//		fmt.Println("true or false:", flag)
+//		token, err = handle.GenToken(jwt.NewUserdata().SetUserId("huang").SetOrgunitId("100"))
+//		fmt.Println(token, err)
+//		claims, err = jwt.ParseToken(token)
+//		fmt.Println(claims, err)
+//
+//		flag = jwt.ValidToken(token)
+//		fmt.Println("true or false:", flag)
+//	}
+package jwt
