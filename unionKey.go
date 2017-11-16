bde8917bfb8e5ece9917a31722e34c25c175b64e
hzwy23 <hzwy23@163.com>
2017-11-17 00:38:54 +0800

    add comments

--- unionKey.go
+++ unionKey.go
@@ -10,6 +10,8 @@ const (
 	separator = 0x1f
 )
 
+
+// function JoinKey 联合主键拼接，将多个key拼接成一个字符串，默认分隔符是0x1f
 func JoinKey(str ...string) string{
 	size := len(str)
 	result := ""
@@ -19,6 +21,10 @@ func JoinKey(str ...string) string{
 	return result
 }
 
+// GetKey从联合主键字符串中获取指定顺序的字符串，
+// index 为0时，获取第一个key，
+// index 大于0且小于联合主键中key个数时，返回序号为index的key，index == 1 表示返回第一个key，
+// 当index大于联合主键key个数时，error返回一场信息。
 func GetKey(str string,index int) (string,error) {
    	s:=strings.Split(str,string(separator))
    	if len(s) == 0{
