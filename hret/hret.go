c9ab8453fc818db441c2b6e0e2693789f5fcce99
hzwy23 <hzwy23@163.com>
2017-11-20 23:23:29 +0800

    add log

--- hret/hret.go
+++ hret/hret.go
@@ -1,63 +1,59 @@
 package hret
 
 import (
+	"github.com/hzwy23/panda/logger"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 )
 
-type HttpOkMsg struct {
+type RetContent struct {
 	Version    string      `json:"version"`
-	Reply_code int         `json:"reply_code"`
-	Reply_msg  string      `json:"reply_msg"`
+	Code       int         `json:"code"`
+	Message    string      `json:"msg"`
+	Details    interface{} `json:"details"`
 	Data       interface{} `json:"data,omitempty"`
 	Total      int64       `json:"total,omitempty"`
 	Rows       interface{} `json:"rows,omitempty"`
 }
 
-type HttpErrMsg struct {
-	Error_code    int         `json:"error_code"`
-	Error_msg     string      `json:"error_msg"`
-	Error_details interface{} `json:"error_details,omitempty"`
-	Version       string      `json:"version"`
-}
-
-func Json(w http.ResponseWriter, data interface{}) ([]byte, error) {
+// 将数据打包成json格式，返回给客户端，
+// 返回数据成功，error为nil，否则为错误信息
+func Json(w http.ResponseWriter, data interface{}) (error) {
 	ijs, err := json.Marshal(data)
 	if err != nil {
-		fmt.Println(err)
+		logger.Error(err)
 		w.WriteHeader(http.StatusExpectationFailed)
-		w.Write([]byte(`{error_code:765,error_msg:"` + err.Error() + `",error_details:"format json type info failed."}`))
-		return ijs, err
+		w.Write([]byte(`{code:765,error_msg:"` + err.Error() + `",details:"format json type info failed."}`))
+		return  err
 	}
 	if string(ijs) == "null" {
 		w.Write([]byte("[]"))
-		return ijs, nil
+		return nil
 	}
 	_, err = w.Write(ijs)
-	return ijs, err
+	return  err
 }
 
 func Error(w http.ResponseWriter, code int, msg string, details ...interface{}) {
-	e := HttpErrMsg{
-		Error_code:    code,
-		Error_msg:     msg,
-		Error_details: details,
+	e := RetContent{
+		Code:        code,
+		Message:     msg,
+		Details:     details,
 	}
 	writHttpError(w, e)
 }
 
 func Success(w http.ResponseWriter, v interface{}) {
-	ok := HttpOkMsg{
-		Version:    "v1.0",
-		Reply_code: 200,
-		Reply_msg:  "execute successfully.",
-		Data:       v,
+	ok := RetContent{
+		Version:   "v1.0",
+		Code:      200,
+		Message:   "execute successfully.",
+		Data:      v,
 	}
 	ojs, err := json.Marshal(ok)
 	if err != nil {
-		fmt.Println(err)
+		logger.Error(err)
 		w.WriteHeader(http.StatusExpectationFailed)
 		w.Write([]byte(`{error_code:` + strconv.Itoa(http.StatusExpectationFailed) + `,error_msg:"format json type info failed.",error_details:"format json type info failed."}`))
 		return
@@ -66,18 +62,18 @@ func Success(w http.ResponseWriter, v interface{}) {
 	return
 }
 
-func BootstrapTableJson(w http.ResponseWriter, total int64, v interface{}) {
-	ok := HttpOkMsg{
+func BootstrapTable(w http.ResponseWriter, total int64, v interface{}) {
+	ok := RetContent{
 		Version:    "v1.0",
-		Reply_code: 200,
-		Reply_msg:  "execute successfully.",
+		Code: 200,
+		Message:  "execute successfully.",
 		Rows:       v,
 		Total:      total,
 	}
 
 	ijs, err := json.Marshal(ok)
 	if err != nil {
-		fmt.Println(err)
+		logger.Error(err)
 		w.WriteHeader(http.StatusExpectationFailed)
 		w.Write([]byte(`{error_code:` + strconv.Itoa(http.StatusExpectationFailed) + `,error_msg:"format json type info failed.",error_details:"format json type info failed."}`))
 		return
@@ -85,15 +81,15 @@ func BootstrapTableJson(w http.ResponseWriter, total int64, v interface{}) {
 	w.Write(ijs)
 }
 
-func writHttpError(w http.ResponseWriter, herr HttpErrMsg) {
+func writHttpError(w http.ResponseWriter, herr RetContent) {
 	herr.Version = "v1.0"
 	ijs, err := json.Marshal(herr)
 	if err != nil {
-		fmt.Println(err)
+		logger.Error(err)
 		w.WriteHeader(http.StatusExpectationFailed)
-		w.Write([]byte(`{error_code:` + strconv.Itoa(herr.Error_code) + `,error_msg:"` + herr.Error_msg + `",error_details:"format json type info failed."}`))
+		w.Write([]byte(`{code:` + strconv.Itoa(herr.Code) + `,msg:"` + herr.Message + `",details:"format json type info failed."}`))
 		return
 	}
-	w.WriteHeader(herr.Error_code)
+	w.WriteHeader(herr.Code)
 	w.Write(ijs)
 }
