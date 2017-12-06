3ed2e61adad962cd02e9867dd8980ddfc8779e35
hzwy23 <hzwy23@163.com>
2017-12-06 17:03:24 +0800

    add function DateFormat

--- currentTime.go
+++ currentTime.go
@@ -1,6 +1,7 @@
 package panda
 
 import "time"
+import "errors"
 
 // CurTime获取系统当前时间，时间格式是：yyyy-mm-dd h24:mm:ss
 func CurTime() string {
@@ -10,4 +11,28 @@ func CurTime() string {
 // CurDate获取系统当前日前，日期格式是： yyyy-mm-dd
 func CurDate() string {
 	return time.Now().Format("2006-01-02")
+}
+
+func DateFormat(date string, tag string) (string,error){
+	if tag == "YYYY-MM-DD" {
+		t,err:=time.Parse("2006-01-02",date)
+		if err != nil {
+			return "",err
+		}
+		return t.Format("2006-01-02"),nil
+	} else if tag == "YYYY-MM-DD HH24:MM:SS" {
+		t,err:=time.Parse("2006-01-02 15:04:05",date)
+		if err != nil {
+			return "",err
+		}
+		return t.Format("2006-01-02 15:04:05"),nil
+	} else if tag == "YYYY-MM-DD HH:MM:SS" {
+		t,err:=time.Parse("2006-01-02 03:04:05",date)
+		if err != nil {
+			return "",err
+		}
+		return t.Format("2006-01-02 03:04:05"),nil
+	} else {
+		return "",errors.New("format is wrong.")
+	}
 }
--- currentTime_test.go
+++ currentTime_test.go
@@ -1,12 +1,13 @@
 package panda_test
 
 import (
-	"fmt"
 	"github.com/hzwy23/panda"
+	"fmt"
 	"testing"
 )
 
-func TestCurTime(t *testing.T) {
-	fmt.Println(panda.CurTime())
-	fmt.Println(panda.CurDate())
+func TestDateFormat(t *testing.T){
+	fmt.Println(panda.DateFormat("2017-12-06","YYYY-MM-DD"))
+	fmt.Println(panda.DateFormat("2017-12-06 11:03:21","YYYY-MM-DD HH:MM:SS"))
+	fmt.Println(panda.DateFormat("2017-12-06 21:03:21","YYYY-MM-DD HH24:MM:SS"))
 }
